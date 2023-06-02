28ae650416101210822775fa599c9c24b2c7de3e
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2023-06-01 21:04:28 -0400

    make it fast

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,22 @@ use stl::Triangle;
 struct Cli {
     path: std::path::PathBuf,
 
-    #[clap(long="output-folder", short="o")]
+    #[clap(long="output-folder")]
     output_folder: Option<std::path::PathBuf>,
 }
+
+type Vertex = [OrderedFloat<f32>; 3];
+
+
+fn get_vertices(t: &Triangle) -> [Vertex; 3] {
+    return [
+        [OrderedFloat(t.v1[0]), OrderedFloat(t.v1[1]), OrderedFloat(t.v1[2])],
+        [OrderedFloat(t.v2[0]), OrderedFloat(t.v2[1]), OrderedFloat(t.v2[2])],
+        [OrderedFloat(t.v3[0]), OrderedFloat(t.v3[1]), OrderedFloat(t.v3[2])],
+    ]
+}
+
+
 
 fn triangles_connected(a: &Triangle, b: &Triangle) -> bool {
     for p1 in [a.v1, a.v2, a.v3] {
@@ -35,6 +48,55 @@ fn copy_triangle(t: &stl::Triangle) -> stl::Triangle {
     }
 }
 
+fn find_connected_sets(triangles: &[Triangle]) -> Vec<Vec<Triangle>> {
+    let mut vertex_to_triangles: HashMap<Vertex, Vec<usize>> = HashMap::new();
+    let mut visited: HashSet<usize> = HashSet::new();
+    let mut connected_sets: Vec<Vec<Triangle>> = Vec::new();
+
+    // Step 2: Build the graph
+    for (i, triangle) in triangles.iter().enumerate() {
+        for vertex in get_vertices(triangle) {
+            vertex_to_triangles
+                .entry(vertex)
+                .or_insert_with(Vec::new)
+                .push(i);
+        }
+    }
+
+    // Step 4: Find connected components using DFS with a stack
+    for (i, triangle) in triangles.iter().enumerate() {
+        if !visited.contains(&i) {
+            let mut connected_set: Vec<Triangle> = Vec::new();
+            let mut stack: VecDeque<usize> = VecDeque::new();
+            stack.push_back(i);
+
+            while let Some(current) = stack.pop_back() {
+                if visited.contains(&current) {
+                    continue;
+                }
+
+                visited.insert(current);
+                let current_triangle = &triangles[current];
+                connected_set.push(copy_triangle(current_triangle));
+
+                for vertex in get_vertices(current_triangle) {
+                    if let Some(adjacent_triangles) = vertex_to_triangles.get(&vertex) {
+                        for &adjacent in adjacent_triangles {
+                            if !visited.contains(&adjacent) {
+                                stack.push_back(adjacent);
+                            }
+                        }
+                    }
+                }
+            }
+
+            connected_sets.push(connected_set);
+        }
+    }
+
+    connected_sets
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args = Cli::parse();
 
@@ -46,44 +108,8 @@ fn main() -> Result<(), std::io::Error> {
 
     let mut graph: Vec<Vec<usize>> = vec![Vec::new(); stl.triangles.len()];
     let mut visited: Vec<bool> = vec![false; stl.triangles.len()];
-    let mut connected_sets: Vec<Vec<Triangle>> = Vec::new();
+    let connected_sets = find_connected_sets(&stl.triangles);
 
-    // Step 2: Build the graph
-    for (i, triangle1) in stl.triangles.iter().enumerate() {
-        for (j, triangle2) in triangles.iter().enumerate() {
-            if i == j {
-                continue;
-            }
-
-            if triangles_connected(triangle1, triangle2) {
-                graph[i].push(j);
-                graph[j].push(i);
-            }
-        }
-    }
-
-    // Step 4: Find connected components using DFS with a stack
-    for (i, triangle) in stl.triangles.iter().enumerate() {
-        if !visited[i] {
-            let mut connected_set: Vec<Triangle> = Vec::new();
-            let mut stack: VecDeque<usize> = VecDeque::new();
-            stack.push_back(i);
-
-            while let Some(current) = stack.pop_back() {
-                visited[current] = true;
-                connected_set.push(copy_triangle(&stl.triangles[current]));
-
-                let neighbors = &graph[current];
-                for neighbor in neighbors.iter() {
-                    if !visited[*neighbor] {
-                        stack.push_back(*neighbor);
-                    }
-                }
-            }
-
-            connected_sets.push(connected_set);
-        }
-    }
 
 
     let base_filename = args.path.file_stem().unwrap();
